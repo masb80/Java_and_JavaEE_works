3cdbbb032cbec7702ec9f5033d8c273efa50687c
Samad Bhuiyan <samad1bhuiyan@gmail.com>
2021-02-25 16:09:28 -0400

    array manipulation

--- coding_challenge/HackerRank/Datastructures/ArrayManipulation/ArrayManipulation.java
+++ coding_challenge/HackerRank/Datastructures/ArrayManipulation/ArrayManipulation.java
@@ -1,15 +1,45 @@
 package ArrayManipulation;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedWriter;
 import java.util.Scanner;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.util.stream.Collectors;
 
 
 public class ArrayManipulation {
+      // Complete the arrayManipulation function below.
+      static long arrayManipulation(int n, int[][] queries) {
+        int queryrowsize = queries.length;
+        int [][] arr = new int [queries.length][n];
+        for(int i =0; i < queryrowsize; i++){
+            for(int j = 0; j < n; j++){
+                if(j >= queries[i][0] && j <= queries[i][1] ){
+                        arr[i][j] = arr[i][j] + queries[i][2];
+                }
+            }
+        }
+        long maxcolTop = 0L;
+        for(int i =0; i < arr[0].length; i++){
+            long maxcol = 0L;
+            for(int j = 0; j < arr.length; j++){                
+                maxcol += arr[j][i]; 
+                if(maxcol > maxcolTop ){
+                    maxcolTop = maxcol;
+                }
+            }
+        }
+        return maxcolTop;
 
-    public static void main(String[] args){
-
-        Scanner in = new Scanner(System.in);
+        /*
+        // this also worked
+        
+              Scanner in = new Scanner(System.in);
 
         int N = in.nextInt();
         int M = in.nextInt();
@@ -38,5 +68,48 @@ public class ArrayManipulation {
         }
         
         System.out.println(max);
+
+
+        */
     }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+    //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+    // String[] nm = new String[2];
+    // for(int i =0; i<2;i++){
+    //      nm = scanner.nextLine().split(" ");
+         
+    // }
+
+    // int n = Integer.parseInt(nm[0]); // column or  I need to make an array of that size of column
+
+    // int m = Integer.parseInt(nm[1]); // row / how many queries entered
+    int n = 5; // column or  I need to make an array of that size of column
+
+    int m = 3;
+    int[][] queries = new int[m][3]; // row = m, column = 3
+
+    for (int i = 0; i < m; i++) {
+        //String[] queriesRowItems = scanner.nextLine().split(" ");
+      //  String[] queriesRowItems = scanner.nextInt();
+       // scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int j = 0; j < 3; j++) {
+          //  int queriesItem = Integer.parseInt(queriesRowItems[j]);
+            queries[i][j] = scanner.nextInt();
+        }
+    }
+
+    long result = arrayManipulation(n, queries);
+
+   // bufferedWriter.write(String.valueOf(result));
+  //  bufferedWriter.newLine();
+
+   // bufferedWriter.close();
+
+    scanner.close();
+}  
+    
 }
